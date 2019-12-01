e6a14fb8b186c42b9e689788132293498bfcb154
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-12-01 20:39:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,10 +35,7 @@ def monopoly_with_third_tier
 	       :three_pieces_owned=>100,
 	       :four_pieces_owned=>200
 	    },
-	    :names=> 
-	    { 
-	     :reading_railraod=> 
-	    }
+	     :reading_railraod=> {}
 	  } 
 	} 
 end
