74e1e4e040db881209b4af591a532b66902f2c8f
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-12-01 20:46:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,7 +52,9 @@ def monopoly_with_fourth_tier
 	      :reading_railroad=>{
 	         "mortgage_value"=>"$100"
 	      }, 
-	       :pennsylvania_railroad=>{}, 
+	       :pennsylvania_railroad=>{
+	         
+	       }, 
 	        :b_and_o_railroad=>{},
 	         :shortline=>{}
 	    }, 
